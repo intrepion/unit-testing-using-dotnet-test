f1146a53765f38b41b3a38d20e3d73b318771d4f
Oliver Forral <intrepion@gmail.com>
2019-11-03 16:55:23 -0800

    Changed fact to a theory

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -12,12 +12,18 @@ namespace Prime.UnitTests.Services
             _primeService = new PrimeService();
         }
 
-        [Fact]
-        public void IsPrime_InputIs1_ReturnFalse()
+        [Theory]
+        [InlineData(-256)]
+        [InlineData(-42)]
+        [InlineData(-99)]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
         {
-            var result = _primeService.IsPrime(1);
+            var result = _primeService.IsPrime(value);
 
-            Assert.False(result, "1 should not be prime");
+            Assert.False(result, $"{value} should not be prime");
         }
     }
 }
